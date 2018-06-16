a57d1c44ffe9d360492705f7543a444ced1fb07c
Sarthak Aggarwal <sarthakaggarwal97@gmail.com>
2018-06-17 01:41:14 +0530

    DialogFlow Working

---
+++ app/src/main/java/com/flatfisher/dialogflowchatbotsample/MainActivity.java
@@ -0,0 +1,287 @@
+package com.flatfisher.dialogflowchatbotsample;
+
+import android.app.Application;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import com.github.bassaer.chatmessageview.model.Message;
+import com.github.bassaer.chatmessageview.view.ChatView;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.mapzen.speakerbox.Speakerbox;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import ai.api.AIServiceException;
+import ai.api.RequestExtras;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIDataService;
+import ai.api.android.GsonFactory;
+import ai.api.model.AIContext;
+import ai.api.model.AIError;
+import ai.api.model.AIEvent;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+import ai.api.model.Metadata;
+import ai.api.model.Result;
+import ai.api.model.Status;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextToSpeech.OnInitListener {
+
+    public static final String TAG = MainActivity.class.getName();
+    private Gson gson = GsonFactory.getGson();
+    private AIDataService aiDataService;
+    private ChatView chatView;
+    private User myAccount;
+    private User droidKaigiBot;
+
+    private TextToSpeech tts;
+  //  Speakerbox speakerbox ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Application activity = new Application();
+//        speakerbox = new Speakerbox(activity);
+        initChatView();
+        tts = new TextToSpeech(this, this);
+
+        //Language, Dialogflow Client access token
+        final LanguageConfig config = new LanguageConfig("en", "3eb7aedf6d104c9681b840fabb81054f");
+        initService(config);
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+    @Override
+    public void onClick(View v) {
+        //new message
+        final Message message = new Message.Builder()
+                .setUser(myAccount)
+                .setRightMessage(true)
+                .setMessageText(chatView.getInputText())
+                .hideIcon(true)
+                .build();
+        //Set to chat view
+        chatView.send(message);
+        sendRequest(chatView.getInputText());
+        //Reset edit text
+        chatView.setInputText("");
+    }
+
+    /*
+    * AIRequest should have query OR event
+    */
+    private void sendRequest(String text) {
+        Log.d(TAG, text);
+        final String queryString = String.valueOf(text);
+        final String eventString = null;
+        final String contextString = null;
+
+        if (TextUtils.isEmpty(queryString) && TextUtils.isEmpty(eventString)) {
+            onError(new AIError(getString(R.string.non_empty_query)));
+            return;
+        }
+
+        new AiTask().execute(queryString, eventString, contextString);
+    }
+
+    @Override
+    public void onInit(int i) {
+        if (i == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+
+              //  speakOut(speech);
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
+
+    public class AiTask extends AsyncTask<String, Void, AIResponse> {
+        private AIError aiError;
+
+        @Override
+        protected AIResponse doInBackground(final String... params) {
+            final AIRequest request = new AIRequest();
+            String query = params[0];
+            String event = params[1];
+            String context = params[2];
+
+            if (!TextUtils.isEmpty(query)){
+                request.setQuery(query);
+            }
+
+            if (!TextUtils.isEmpty(event)){
+                request.setEvent(new AIEvent(event));
+            }
+
+            RequestExtras requestExtras = null;
+            if (!TextUtils.isEmpty(context)) {
+                final List<AIContext> contexts = Collections.singletonList(new AIContext(context));
+                requestExtras = new RequestExtras(contexts, null);
+            }
+
+            try {
+                return aiDataService.request(request, requestExtras);
+            } catch (final AIServiceException e) {
+                aiError = new AIError(e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final AIResponse response) {
+            if (response != null) {
+                onResult(response);
+            } else {
+                onError(aiError);
+            }
+        }
+    }
+
+
+    private void onResult(final AIResponse response) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // Variables
+                gson.toJson(response);
+                final Status status = response.getStatus();
+                final Result result = response.getResult();
+                final String speech = result.getFulfillment().getSpeech();
+                final Metadata metadata = result.getMetadata();
+                final HashMap<String, JsonElement> params = result.getParameters();
+
+                // Logging
+                Log.d(TAG, "onResult");
+                Log.i(TAG, "Received success response");
+                Log.i(TAG, "Status code: " + status.getCode());
+                Log.i(TAG, "Status type: " + status.getErrorType());
+                Log.i(TAG, "Resolved query: " + result.getResolvedQuery());
+                Log.i(TAG, "Action: " + result.getAction());
+                Log.i(TAG, "Speech: " + speech);
+
+                if (metadata != null) {
+                    Log.i(TAG, "Intent id: " + metadata.getIntentId());
+                    Log.i(TAG, "Intent name: " + metadata.getIntentName());
+                }
+
+                if (params != null && !params.isEmpty()) {
+                    Log.i(TAG, "Parameters: ");
+                    for (final Map.Entry<String, JsonElement> entry : params.entrySet()) {
+                        Log.i(TAG, String.format("%s: %s",
+                                entry.getKey(), entry.getValue().toString()));
+                    }
+                }
+
+
+                //Update view to bot says
+                final Message receivedMessage = new Message.Builder()
+                        .setUser(droidKaigiBot)
+                        .setRightMessage(false)
+                        .setMessageText(speech)
+                        .build();
+                chatView.receive(receivedMessage);
+
+                speakOut(speech);
+                if(speech.equals("Booking a cab to the nearest hospital now !")) {
+                    PackageManager pm = getPackageManager();
+                    try {
+                        pm.getPackageInfo("com.ubercab", PackageManager.GET_ACTIVITIES);
+                        String uri = "uber://?action=setPickup&pickup=my_location&dropoff[latitude]=65.0070&dropoff[longitude]=25.5181&dropoff[nickname]= Oulun yliopistollinen sairaala";
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(uri));
+                        startActivity(intent);
+                    } catch (PackageManager.NameNotFoundException e) {
+                        try {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.ubercab")));
+                        } catch (android.content.ActivityNotFoundException anfe) {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=com.ubercab")));
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void speakOut(String text) {
+
+
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    private void onError(final AIError error) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG,error.toString());
+            }
+        });
+    }
+
+    private void initChatView() {
+        int myId = 0;
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_user);
+        String myName = "Shivam";
+        myAccount = new User(myId, myName, icon);
+
+        int botId = 1;
+        String botName = "Dr Ayush";
+        droidKaigiBot = new User(botId, botName, icon);
+
+        chatView = findViewById(R.id.chat_view);
+        chatView.setRightBubbleColor(ContextCompat.getColor(this, R.color.green500));
+        chatView.setLeftBubbleColor(Color.WHITE);
+        chatView.setBackgroundColor(ContextCompat.getColor(this, R.color.blueGray500));
+        chatView.setSendButtonColor(ContextCompat.getColor(this, R.color.lightBlue500));
+        chatView.setSendIcon(R.drawable.ic_action_send);
+        chatView.setRightMessageTextColor(Color.WHITE);
+        chatView.setLeftMessageTextColor(Color.BLACK);
+        chatView.setUsernameTextColor(Color.WHITE);
+        chatView.setSendTimeTextColor(Color.WHITE);
+        chatView.setDateSeparatorColor(Color.WHITE);
+        chatView.setInputTextHint("Ask for medical help! ");
+        chatView.setMessageMarginTop(5);
+        chatView.setMessageMarginBottom(5);
+        chatView.setOnClickSendButtonListener(this);
+    }
+
+    private void initService(final LanguageConfig languageConfig) {
+        final AIConfiguration.SupportedLanguages lang =
+                AIConfiguration.SupportedLanguages.fromLanguageTag(languageConfig.getLanguageCode());
+        final AIConfiguration config = new AIConfiguration(languageConfig.getAccessToken(),
+                lang,
+                AIConfiguration.RecognitionEngine.System);
+        aiDataService = new AIDataService(this, config);
+    }
+}
